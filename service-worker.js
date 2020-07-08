ff2d76ea8d534cdaa5962543af9b0458a3ff4674
Stephane <oklaova@gmail.com>
2020-07-08 00:02:09 -0400

    using janus livestream

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8a93632cca6ef5d25d73eb104b03119.js"
+  "/precache-manifest.7f9d509250bee4b70243538b5da5b35a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "digitalellc"});
